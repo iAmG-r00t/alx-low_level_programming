b210bc0f6692e3f6943446c09080c44a47eed7f0
iAmG-r00t <numimickey2@gmail.com>
2021-06-17 12:27:20 -0700

    6th challenge minor edit, make betty happy 🧞 👨‍💻 🦹 🔥

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints the size of various types on the computer it is compiled and run on.
+ * Description: using sizeof to print the size of various types.
  *
  * Return: Always 0 (Succesful)
 */
