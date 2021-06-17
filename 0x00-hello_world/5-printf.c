88befcc48683ebe5cfc0a12ab89904cb5085573e
iAmG-r00t <numimickey2@gmail.com>
2021-06-17 11:52:21 -0700

    5th challenge minor edit 🧞 👨‍💻 🦹 🔥

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	getchar();
 	return (0);
 }
