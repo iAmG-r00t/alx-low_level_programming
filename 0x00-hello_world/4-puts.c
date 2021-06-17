6a1bd768ba775f81ed112efc29dcb1ac2a05f90d
iAmG-r00t <numimickey2@gmail.com>
2021-06-17 11:35:11 -0700

    minor edit for challenge 4 to match betty syntax 🧞 👨‍💻 🦹 🔥

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-/*
+/**
+ * main - Entry point
  *
- * A C program that prints with put function
+ * Description: A C program that prints with put function
  *
+ * Return: Always 0 (Success)
 */
 
 int main(void)
