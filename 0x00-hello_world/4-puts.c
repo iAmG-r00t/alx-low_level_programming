80dfa5f8f1c062a169f3fe66db825e266f80e2fc
iAmG-r00t <numimickey2@gmail.com>
2021-06-17 12:30:19 -0700

    just realized I don't need to use getchar .. 🧞 👨‍💻 🦹 🔥

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,6 +11,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	getchar();
 	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -11,6 +11,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	getchar();
 	return (0);
 }
