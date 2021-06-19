8faceacd089ddf3907e618d817f2b22d67651fea
iAmG-r00t <numimickey2@gmail.com>
2021-06-19 05:03:40 -0700

    rectify code logic 1st challenge 🧞 👨‍💻 🦹 🔥

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,22 +14,26 @@
 int main(void)
 {
 	int n;
+	int lD;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
+	/*get the last digit of value of n*/
+	lD = n % 10;
+
 	/* if statement */
-	if (n == 0)
+	if (lD == 0)
 	{
-		printf("Last digit of %d and is 0\n", n);
+		printf("Last digit of %d is %d and is 0\n", n, lD);
 	}
-	else if (n > 5)
+	else if (lD > 5)
 	{
-		printf("Last digit of %d and is greater than 5\n", n);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lD);
 	}
-	else if (n < 6 && n != 0)
+	else if (lD < 6 && lD != 0)
 	{
-		printf("Last digit of %d and is less than 6 and not 0\n", n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lD);
 	}
 
 	return (0);
