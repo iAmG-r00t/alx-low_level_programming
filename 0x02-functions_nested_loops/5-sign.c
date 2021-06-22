7aefc551912f8005bb9c20d71dd63addfcef023a
iAmG-r00t <numimickey2@gmail.com>
2021-06-22 12:20:44 -0700

    5th challenge fix betty styling code 🧞 👨‍💻 🦹 🔥

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,15 +12,18 @@
 
 int print_sign(int n)
 {
-	if (n > 0) {
+	if (n > 0)
+	{
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 0) {
+	else if (n == 0)
+	{
 		_putchar('0');
 		return (0);
 	}
-	else {
+	else
+	{
 		_putchar('-');
 		return (-1);
 	}
