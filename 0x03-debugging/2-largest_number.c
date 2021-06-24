f1df314e1df27a672981c460e9379300cdaf6b05
iAmG-r00t <numimickey2@gmail.com>
2021-06-24 02:02:12 -0700

    fix 2nd challenge 🧞 👨‍💻 🦹 🔥

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,11 +12,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
