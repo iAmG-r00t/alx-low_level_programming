9c6a70ab45b4069e3af3e8b682e903a2d445ca24
iAmG-r00t <numimickey2@gmail.com>
2021-06-24 12:37:03 -0700

    Some minor fix 🧞 👨‍💻 🦹 🔥

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	if (year % 4 == 0 || ((year % 400 == 0) && (year % 100 == 0)))
 	{
-		if (month >= 2 && day >= 60)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
