1e5440192ae8ca7432003fb3e0edaf5ddbe8ba47
iAmG-r00t <numimickey2@gmail.com>
2021-06-25 11:35:22 -0700

    added some comments, better understanding 🧞 👨‍💻 🦹 🔥

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,8 +12,19 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 4 == 0 || ((year % 400 == 0) && (year % 100 == 0)))
+	/**
+	 * check is the year is a leap year, where it is a leap year
+	 * if the year can be divided by 4 evenly, and if the year
+	 * can be divided evenly by 100 then its not a leap year
+	 * unless it is divisible by 400
+	*/
+	if (year % 4 == 0 && ((year % 100 == 0) && (year % 400 == 0)))
 	{
+		/**
+		 * if its a leap year then check if the month
+		 * is february and if its days are larger or
+		 * equal to 60 and if it is add one day
+		*/
 		if (month > 2 && day >= 60)
 		{
 			day++;
@@ -23,8 +34,12 @@ void print_remaining_days(int month, int day, int year)
 	printf("Remaining days: %d\n", 366 - day);
 
 	}
+	/*if its not a leap year then ..*/
 	else
 	{
+		/* check if month is february and if days
+		 * are equal to 60 then its an invalid date
+		*/
 		if (month == 2 && day == 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
