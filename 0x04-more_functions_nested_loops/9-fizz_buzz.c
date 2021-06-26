56ce71eee20a5041ecb75ef3878ded3c17328e6b
iAmG-r00t <numimickey2@gmail.com>
2021-06-26 06:21:25 -0700

    9th challenge minor edit 🧞 👨‍💻 🦹 🔥

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,21 +18,29 @@ int main(void)
 	{
 		if ((i % 3 == 0) && !(i % 5 == 0))
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if ((i % 5 == 0) && !(i % 3 == 0))
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
+		}
+
+		if (i != 100)
+		{
+			printf(" ");
+		}
+		else
+		{
+			printf("\n");
 		}
 	}
-	printf("\n");
 	return (0);
 }
