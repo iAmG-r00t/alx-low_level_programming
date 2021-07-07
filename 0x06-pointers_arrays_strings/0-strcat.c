ceb0828eab6c790752f818cb62c5ce93429e6822
iAmG-r00t <numimickey2@gmail.com>
2021-07-06 18:19:57 -0700

    Some minor code logic edit 🧞 👨‍💻 🦹 🔥

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,19 +14,14 @@ char *_strcat(char *dest, char *src)
 
 	c = 0;
 	/*find the size of dest array*/
-	while (c >= 0)
-	{
-		if (dest[c] == '\0')
-			break;
+	while (dest[c])
 		c++;
-	}
 
 	/* iterate through each src array value without the null byte*/
-	for (c2 = 0; c2 >= 0 && src[c2] != '\0'; )
+	for (c2 = 0; src[c2] ; c2++)
 	{
-		/*append src[c2] to dest[c + c2]*/
-		dest[c + c2] = src[c2];
-		c2++;
+		/*append src[c2] to dest[c] while overwritting the null byte in dest*/
+		dest[c++] = src[c2];
 	}
 	return (dest);
 }
