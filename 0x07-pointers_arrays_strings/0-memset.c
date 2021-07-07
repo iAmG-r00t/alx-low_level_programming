72dfd034214a78ab18abd7accf95d8af3077abd8
iAmG-r00t <numimickey2@gmail.com>
2021-07-07 06:33:15 -0700

    add a comment 🧞 👨‍💻 🦹 🔥

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -19,6 +19,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
+	/**
+	 * declare an unsigned int
+	 * because we are storing a
+	 * value that will always be
+	 * non-negative (zero or positive)
+	*/
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
