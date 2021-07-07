3e789c2342110acfe24f2d474f6fe47d1152f768
iAmG-r00t <numimickey2@gmail.com>
2021-07-07 05:32:41 -0700

    adding somecommentsn 🧞 👨‍💻 🦹 🔥

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,14 +11,24 @@ char *cap_string(char *s)
 {
 	int i = 0;
 
+	/*iterate through our array values*/
 	while (s[i] != '\0')
 	{
+		/*check for any lowercase letters*/
 		if (s[i] >= 97 && s[i] <= 122)
 		{
+			/**
+			 * if we have a null character
+			 * change its value to capital
+			*/
 			if (i == 0)
 			{
 				s[i] -= 32;
 			}
+			/**
+			 * if we find any character matching the below before any small
+			 * letter we change that value to a capital letter.
+			*/
 			if (s[i - 1] == 32 || s[i - 1] == 9 || s[i - 1] == 10 ||
 				s[i - 1] == 44 || s[i - 1] == 59 || s[i - 1] == 46 ||
 				s[i - 1] == 33 || s[i - 1] == 63 || s[i - 1] == 34 ||
