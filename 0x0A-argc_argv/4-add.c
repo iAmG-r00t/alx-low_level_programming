e1087954adcf4d0c64be1767681711f8fcd83976
iAmG-r00t <numimickey2@gmail.com>
2021-07-13 11:38:06 -0700

    Fix 4th challenge 🧞 👨‍💻 🦹 🔥

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
+
+/**
+ * isNum - check if string array is num
+ * @num: string to check
+ * Return: 0 if it's a number
+ *         1 if it's not a number
+*/
+
+int isNum(char num[])
+{
+	int i, l = strlen(num);
+
+	for (i = 0; i < l; i++)
+	{
+		if(!isdigit(num[i]))
+			return (1);
+	}
+	return (0);
+}
+
 
 /**
  * main - a program that adds positive numbers
@@ -24,7 +45,7 @@ int main(int argc, char *argv[])
 		sum = 0;
 		for (i = 1; i < argc; i++)
 		{
-			if (isdigit(*argv[i]))
+			if (isNum(argv[i]) == 0)
 			{
 				sum += atoi(argv[i]);
 			}
