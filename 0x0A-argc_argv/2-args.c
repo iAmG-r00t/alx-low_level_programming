468df0034944d49e9e8438377950601a6ef9a76e
iAmG-r00t <numimickey2@gmail.com>
2021-07-13 05:18:15 -0700

    2nd challenge comment edit 🧞 👨‍💻 🦹 🔥

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,8 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - a program that prints the number of arguments
- *        passed into it
+ * main - a program that prints all arguments it receives
  *
  * @argc: holds the number of arguments passed
  * @argv: array pointer that holds the arguments passed
