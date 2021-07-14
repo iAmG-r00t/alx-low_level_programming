1c8865e84ff27e3adbcf1e3d4c1c91ca91807932
iAmG-r00t <numimickey2@gmail.com>
2021-07-14 04:05:13 -0700

    Fixed code logic 🧞 👨‍💻 🦹 🔥

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,12 +15,13 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *a;
 
+	if (size == 0)
+		return (NULL);
+
 	a = malloc(size * sizeof(char));
 
-	if (size == 0 || a == NULL)
-	{
+	if (a == NULL)
 		return (NULL);
-	}
 
 	for (i = 0; i < size; i++)
 		a[i] = c;
