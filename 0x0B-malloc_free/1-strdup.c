b1ac207fe660a898bfb61bf13509b1064164d7b0
iAmG-r00t <numimickey2@gmail.com>
2021-07-14 04:12:57 -0700

    Fixed code logic 🧞 👨‍💻 🦹 🔥

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,23 +11,21 @@
 
 char *_strdup(char *str)
 {
-	int i = 0, length;
+	int i = 0, l = 0;
 	char *s;
 
 	if (str == NULL)
 		return (NULL);
 
 	/*calculate size of str*/
-	while (str[i] != '\0')
-		i++;
+	while (str[l] != '\0')
+		l++;
 
-	length = i;
-
-	s = malloc(length * sizeof(char));
+	s = malloc((l + 1) * sizeof(char));
 	if (s == NULL)
 		return (NULL);
-	i = 0;
-	while (i <= length)
+
+	while (str[i] != '\0')
 	{
 		s[i] = str[i];
 		i++;
