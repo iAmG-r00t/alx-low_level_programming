79154cbd4f64ace4a18f46880383e9f92523ad62
iAmG-r00t <numimickey2@gmail.com>
2021-07-15 11:28:40 -0700

    Fix code logic 🧞 👨‍💻 🦹 🔥

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,7 +13,7 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int l, l1, l2, i;
+	unsigned int length, l1, l2, i, j;
 	char *str;
 
 	/*treat NULL as empty string*/
@@ -22,26 +22,32 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	l1 = l2 = 0;
-	/*length of both s1 & s2*/
+	length = l1 = l2 = 0;
+	/*length of both s1 & s2 respectively*/
 	while (s1[l1] != '\0')
 		l1++;
 	while (s2[l2] != '\0')
 		l2++;
 
+	/*set n to length of s2*/
 	if (n >= l2)
 		n = l2;
 
-	l = l1 + n;
-	str = malloc((l + 1) * sizeof(char));
+	/*s1 length + n*/
+	length = l1 + n;
+
+	str = malloc((length + 1) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
 
-	for (i = 0; i < l; i++)
-		if (i < l)
-			str[i] = s1[i];
-		else
-			str[i] = s2[i - l];
+	/*add s1 to str*/
+	for (i = 0; i < length && s1[i] != '\0'; i++)
+		str[i] = s1[i];
+	for (j = 0; i < length && s2[j] != '\0'; j++)
+	{
+		str[i] = s2[j];
+		i++;
+	}
 	str[i] = '\0';
 
 	return (str);
