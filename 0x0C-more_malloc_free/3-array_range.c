c9f92363118e368b6b119a84c3febe78568d0046
iAmG-r00t <numimickey2@gmail.com>
2021-07-15 14:11:29 -0700

    Well nothing new 🧞 👨‍💻 🦹 🔥

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	array = malloc(((max - min) + 1) * sizeof(int));
+	array = malloc((max - min + 1) * sizeof(int));
 	if (array == NULL)
 		return (NULL);
 
