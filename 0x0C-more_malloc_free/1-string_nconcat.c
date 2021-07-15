0551094f73d3cd0af78ebc7978f2c887d8e5f405
iAmG-r00t <numimickey2@gmail.com>
2021-07-15 12:07:20 -0700

    Fix code logic 🧞 👨‍💻 🦹 🔥

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -41,7 +41,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != '\0'; i++)
 		str[i] = s1[i];
 	/*add s2 n bytes to str*/
-	for (j = 0; j < n; j++)
+	for (j = 0; j < n && str[j] != '\0'; j++)
 	{
 		str[i] = s2[j];
 		i++;
