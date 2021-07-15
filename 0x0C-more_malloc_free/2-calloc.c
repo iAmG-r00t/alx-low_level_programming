67ab5e5fe2655516cc7920d47ec6b1007ed7e055
iAmG-r00t <numimickey2@gmail.com>
2021-07-15 13:21:00 -0700

    Try something 🧞 👨‍💻 🦹 🔥

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -23,7 +23,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; i < (nmemb * size); i++)
+	for (i = 0; i < nmemb; i++)
 		p[i] = 0;
 	return (p);
 }
