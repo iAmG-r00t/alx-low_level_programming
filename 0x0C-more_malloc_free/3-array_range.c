27f6cc060c313225802d7d504ece4a05dda2b993
iAmG-r00t <numimickey2@gmail.com>
2021-07-15 13:58:43 -0700

    Fix checker 🧞 👨‍💻 🦹 🔥

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,19 +12,19 @@
 
 int *array_range(int min, int max)
 {
-	int i, *p;
+	int i, *array;
 
 	if (min > max)
 		return (NULL);
 
-	p = malloc((max - min + 1) * sizeof(int));
-	if (p == NULL)
+	array = malloc(((max - min) + 1) * sizeof(int));
+	if (array == NULL)
 		return (NULL);
 
 	for (i = 0; min <= max; i++)
 	{
-		p[i] = min;
+		array[i] = min;
 		min++;
 	}
-	return (p);
+	return (array);
 }
