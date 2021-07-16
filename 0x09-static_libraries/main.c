fff3192cfc7d8277b2ddbea08c45945a8272cca7
iAmG-r00t <numimickey2@gmail.com>
2021-07-15 19:05:31 -0700

    Fix betty 🧞 👨‍💻 🦹 🔥

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,14 @@
 #include "holberton.h"
 
+/**
+ * main - Entry check alx code
+ *
+ * Return: Always 0 (success)
+*/
+
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	_puts("\"At the end of the day, my goal was
+	       to be the best hacker\"\n\t- Kevin Mitnick");
 	return (0);
 }
