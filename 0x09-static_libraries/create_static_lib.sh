a98c6a87909fc8f062b7ced97cdf3787c468a9f3
iAmG-r00t <numimickey2@gmail.com>
2021-07-15 19:31:33 -0700

    Fix checker 🧞 👨‍💻 🦹 🔥

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -4,15 +4,8 @@
 # called liball.a from all the .c files
 # that are in the current directory
 
-filename = "liball.a" # static library filename
+filename = "liball.a" 
 
-# compile all our .c files
 gcc -c *.c
-# create our static library
 ar rc "$filename" *.o
-# remove all object files
 echo y | rm -I *.o
-# confirm our static library was created
-ar -t "$filename"
-# confirm all our functions were linked correctly
-nm "$filename"
