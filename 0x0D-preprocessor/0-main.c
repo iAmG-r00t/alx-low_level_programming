094880eae2874c19afa7554c1f2dd3e3580bcbba
iAmG-r00t <numimickey2@gmail.com>
2021-07-19 04:02:03 -0700

    0 challenge 🧞 👨‍💻 🦹 🔥

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,18 @@
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	int s;
+
+	s = 98 + SIZE;
+	printf("%d\n", s);
+	return (0);
+}
