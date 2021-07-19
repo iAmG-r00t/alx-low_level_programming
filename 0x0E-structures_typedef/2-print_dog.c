62ad33e1c8f398e2d3243428da9aab9234e9ad2f
iAmG-r00t <numimickey2@gmail.com>
2021-07-19 12:24:02 -0700

    2nd challenge 🧞 👨‍💻 🦹 🔥

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,7 +19,7 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		printf("Name: %s\n", d->name ? d->name : "(nil)");
-		printf("Age: %f\n", d->age);
+		printf("Age: %f\n", d->age ? d->age : "(nil)");
 		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
