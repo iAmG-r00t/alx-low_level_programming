c4907b7fd6da2465f34c4cb444115826abd4222f
iAmG-r00t <numimickey2@gmail.com>
2021-07-19 05:13:14 -0700

    Minor edit on function define name ... 🐱‍🚀💥🔥

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
+#ifndef _OBJECT_LIKE_MACRO_H
+#define _OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
 
--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,5 +1,5 @@
-#ifndef PI_H
-#define PI_H
+#ifndef _PI_H
+#define _PI_H
 
 #define PI 3.14159265359
 
