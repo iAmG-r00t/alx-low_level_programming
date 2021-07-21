89055e7370ef01f2f091aecff563e4b479fecc65
iAmG-r00t <numimickey2@gmail.com>
2021-07-21 16:58:32 -0700

    Fix unkown checker 🧞 👨‍💻 🦹 🔥

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,9 +13,9 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int index;
+	size_t index;
 
-	if (array && action)
+	if (array != NULL && size > 0 && action != NULL)
 		for (index = 0; index < size; index++)
 			action(array[index]);
 }
