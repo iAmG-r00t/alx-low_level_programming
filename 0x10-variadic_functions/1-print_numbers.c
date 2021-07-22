9755a72dd7c23d94f572b85552b97fd51ace6d6f
iAmG-r00t <numimickey2@gmail.com>
2021-07-22 16:52:33 -0700

    Fix checker issues 🧞 👨‍💻 🦹 🔥

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 
 	/* if separator is present */
-	if (*separator)
+	if (separator)
 	{
 		/* iterate through each argument*/
 		while (index < n)
