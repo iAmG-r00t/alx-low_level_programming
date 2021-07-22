74338f6e252ca236c61839a3ca82b3f1667b6753
iAmG-r00t <numimickey2@gmail.com>
2021-07-22 10:13:15 -0700

    Add comments 🧞 👨‍💻 🦹 🔥

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,6 +11,10 @@
  *              a function between the two numbers
  *
  *              Usage: ./calc num1 operator num2
+ *                         |     |       |     |_ argv[3]
+ *                         |     |       |_ argv[2]
+ *                         |     |_ argv[1]
+ *                         |_ argv[0]
  *
  * @argc: argument counter
  * @argv: argument vector
@@ -22,22 +26,25 @@ int main(int argc, char *argv[])
 	int num1, num2;
 	char *operator;
 
+	/* Error if arguments are not 4 with the executable */
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
+	num1 = atoi(argv[1]); /*if argument is string convert to number*/
+	num2 = atoi(argv[3]); /* as stated above */
 	operator = argv[2];
 
+	/* Error if operator is not null or a 2d array '\*' */
 	if (get_op_func(operator) == NULL || operator[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
+	/* Error if user tries to divide / (47) or % (37) by 0*/
 	if ((*operator == 47 || *operator == 37) && num2 == 0)
 	{
 		printf("Error\n");
