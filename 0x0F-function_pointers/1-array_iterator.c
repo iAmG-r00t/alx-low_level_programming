3113bba234106b6df7cf78a021465a24631af814
iAmG-r00t <numimickey2@gmail.com>
2021-07-21 17:00:07 -0700

    Fixbettyr 🧞 👨‍💻 🦹 🔥

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,9 +4,9 @@
  * array_iterator - a function that executes a given function
  *                  as a parameter on each element of an array
  *
- * @array - array to iterate
+ * @array: array to iterate
  * @size: size of array
- * @action: function to print element
+ * @action: pointer to function to call
  *
  * Return: empty if @array and @action is NULL
 */
