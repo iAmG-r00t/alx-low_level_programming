634cfa2dde398efe5eb6ac17bf9a3ad3a5fab274
iAmG-r00t <numimickey2@gmail.com>
2021-07-22 17:30:50 -0700

    Fix betty 🧞 👨‍💻 🦹 🔥

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -27,7 +27,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("(nil)");
 
-		if (separator && index != n -1)
+		if (separator && index != n - 1)
 			printf("%s", separator);
 	}
 	/*clean up*/
