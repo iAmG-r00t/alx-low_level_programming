c07905ace22e7b4ff9812fcfcb1ed959d69f8d1a
iAmG-r00t <numimickey2@gmail.com>
2021-08-01 03:30:28 -0700

    Added some code logic for speed 🧻 🔥 ✍  😎 😂

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,9 @@
 /**
  * _pow_recursion - a function that returns the power of @x by @y x^y
  *
+ * Description: this method is good for speed, check this youtube video
+ *   https://youtu.be/wAyrtLAeWvI?list=PL2_aWCzGMAwLz3g66WrxFGSXvSsvyfzCO
+ *
  * @x: integer x
  * @y: integer y
  *
@@ -8,10 +11,17 @@
 */
 int _pow_recursion(int x, int y)
 {
+	int even;
+
 	if (y < 0)		/*if y is a negative number, return -1*/
 		return (-1);
 	else if (y == 0)	/*x to the power of 0 is 1*/
 		return (1);
-	else			/*get the pow of x by y*/
+	else if (y % 2 == 0)	/*if y is an even number*/
+	{
+		even = _pow_recursion(x, y / 2);
+		return even * even;
+	}
+	else			/*if y is odd*/
 		return (x * _pow_recursion(x, y - 1));
 }
