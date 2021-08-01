a910b443d19180529787b16fa3f833e15e6cc008
iAmG-r00t <numimickey2@gmail.com>
2021-08-01 16:47:08 -0700

    Fix betty 🧻 🔥 ✍  😎 😂

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -20,7 +20,7 @@ int _pow_recursion(int x, int y)
 	else if (y % 2 == 0)	/*if y is an even number*/
 	{
 		even = _pow_recursion(x, y / 2);
-		return even * even;
+		return (even * even);
 	}
 	else			/*if y is odd*/
 		return (x * _pow_recursion(x, y - 1));
