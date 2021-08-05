dec5ddb3ff8490af64d0dc24e00a1cb4b53a4809
iAmG-r00t <numimickey2@gmail.com>
2021-08-05 09:32:20 -0700

    Fix code logic to add new node if idx is not present in list 🧞 👨‍💻 🦹 🔥

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,6 +15,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node, *current;
 	unsigned int index;
 
+	current = *head; /*place first node at current*/
+
 	new_node = malloc(sizeof(listint_t));
 	if ((*head == NULL && idx != 0) || new_node == NULL)
 		return (NULL);
@@ -29,11 +31,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
-
-	if (idx != 0) /*if index where to add our new node is not 0*/
+	else if (idx != 0) /*if index where to add our new node is not 0*/
 	{
-		current = *head; /*place first node at current*/
-
 		/*iterate list to node position idx - 2*/
 		for (index = 0; index < idx - 1; index++)
 			current = current->next;
@@ -43,6 +42,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 		new_node->next = current->next; /*place current node after new node*/
 		current->next = new_node;/*set the new node at index idx*/
+		return (new_node);
+	}
+	else /*if node position is not present in the list*/
+	{
+		new_node->next = NULL;/*set next addr as NULL, indicates last node*/
+		current->next = new_node;/*set the new node at the last position in list*/
 		return (new_node);
 	}
 
