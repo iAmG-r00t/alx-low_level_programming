4408f981ab0e1bf268151e6e51a89c927f9c048c
iAmG-r00t <numimickey2@gmail.com>
2021-08-05 12:24:41 -0700

    fix checker 😠😠

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node, *current;
-	unsigned int index;
+	unsigned int index, nodes;
 
 	current = *head; /*place first node at current*/
 
@@ -23,33 +23,31 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_node->n = n; /* add our element to the new node*/
 
+	/*iterate list to node position idx - 2*/
+	for (index = 0; current != NULL && index < idx - 1; index++)
+	{
+		current = current->next;
+		if (current == NULL)
+			return (NULL);
+	}
+
 	if (idx == 0) /*if the index for new node is 0*/
 	{
 		/*first node will be moved to second node*/
 		new_node->next = *head;
 		/*new node will be placed as the first node*/
 		*head = new_node;
-		return (new_node);
 	}
-	else if (idx != 0) /*if index where to add our new node is not 0*/
+	else if (current->next) /*if index where to add our new node is not 0*/
 	{
-		/*iterate list to node position idx - 2*/
-		for (index = 0; index < idx - 1; index++)
-			current = current->next;
-
-		if (current == NULL)
-			return (NULL);
-
 		new_node->next = current->next; /*place current node after new node*/
 		current->next = new_node;/*set the new node at index idx*/
-		return (new_node);
 	}
 	else /*if node position is not present in the list*/
 	{
 		new_node->next = NULL;/*set next addr as NULL, indicates last node*/
 		current->next = new_node;/*set the new node at the last position in list*/
-		return (new_node);
 	}
 
-	return (NULL);
+	return (new_node);
 }
