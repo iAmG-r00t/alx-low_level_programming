94514b41906dd837a60c1541a843d44a40b1811f
iAmG-r00t <numimickey2@gmail.com>
2021-08-09 10:55:02 -0700

    add holberton.h for 0 challenge 🧞 👨‍💻 🦹 🔥... checker misconfig issue

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "holberton.h"
 
 /**
  * read_textfile - a function that reads a text file and prints it
---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,15 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
