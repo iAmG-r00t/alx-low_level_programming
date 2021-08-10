25635da4db4f17759ab7a2ab1485ab3070f6b648
iAmG-r00t <numimickey2@gmail.com>
2021-08-09 23:06:43 -0700

    clean up code 🧻 🔥 ✍  😎 😂

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,6 +2,35 @@
 
 #define MAXSIZE 1024
 
+
+/**
+ * __exit - prints error messages and exits with exit value
+ * @error: num is either exit value or file descriptor
+ * @s: str is a name, either of the two filenames
+ * @fd: file descriptor
+ * Return: 0 on success
+*/
+int __exit(int error, char *s, int fd)
+{
+	switch (error)
+	{
+		case 97:
+			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+			exit(error);
+		case 98:
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s);
+			exit(error);
+		case 99:
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
+			exit(error);
+		case 100:
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+			exit(error);
+		default:
+			return (0);
+	}
+}
+
 /**
  * main - create a copy of file
  *
@@ -10,44 +39,46 @@
  *
  * Return: 0 for success.
 */
-
 int main(int argc, char *argv[])
 {
-	int file_in, file_out, read_stat;
+	int file_in, file_out;
+	int read_stat, write_stat;
 	int close_in, close_out;
 	char buffer[MAXSIZE];
 
 	/*if arguments are not 3*/
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+		__exit(97, NULL, 0);
 
-	file_in = open(argv[1], O_RDONLY); /*read input file*/
+	/*sets file descriptor for copy from file*/
+	file_in = open(argv[1], O_RDONLY);
 	if (file_in == -1)
+		__exit(98, argv[1], 0);
+
+	/*sets file descriptor for copy to file*/
+	file_out = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (file_out == -1)
+		__exit(99, argv[2], 0);
+
+	/*reads file_in as long as its not NULL*/
+	while ((read_stat = read(file_in, buffer, MAXSIZE)) != 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		if (read_stat == -1)
+			__exit(98, argv[1], 0);
+
+		/*copy and write contents to file_out*/
+		write_stat = write(file_out, buffer, read_stat);
+		if (write_stat == -1)
+			__exit(99, argv[2], 0);
 	}
 
-	file_out = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
-	while ((read_stat = read(file_in, buffer, MAXSIZE)) > 0)
-	{
-		if (file_out == -1 || (write(file_out, buffer, read_stat) != read_stat))
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	}
-
-	if (read_stat < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
-	close_in = close(file_in);
+	close_in = close(file_in); /*close file_in*/
 	if (close_in == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close file %i\n", file_in), exit(100);
+		__exit(100, NULL, file_in);
 
-	close_out = close(file_out);
+	close_out = close(file_out); /*close file_out*/
 	if (close_out == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close file %i\n", file_out), exit(100);
+		__exit(100, NULL, file_out);
 
 	return (0);
 }
--- 0x15-file_io/main/0-main.c
+++ 0x15-file_io/main/0-main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "../main.h"
+#include "../holberton.h"
 
 /**
  * main - check the code for ALX School students.
