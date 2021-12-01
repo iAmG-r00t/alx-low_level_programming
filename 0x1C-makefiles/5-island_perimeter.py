4db2a6ef88ed5e186d5d2e24693f1e03b9804abb
iAmG-r00t <numimickey2@gmail.com>
2021-12-01 13:06:53 +0300

    checker fix 🧞 👨‍💻 🦹 🔥

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines an island perimeter measuring function."""
 
+
 def island_perimeter(grid):
     """Return the perimiter of an island.
     The grid represents water by 0 and land by 1.
