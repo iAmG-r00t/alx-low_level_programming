53cd80450339009a5d7656cb10c04553d86a0687
iAmG-r00t <numimickey2@gmail.com>
2022-02-16 05:58:23 +0300

    fix code logic, minor edit.. 🐙😀😭👌

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,12 +10,12 @@ void print_times_table(int n)
 {
 	int prod, mult, num;
 
-	if (n <= 15 && n > 0)
+	if (n <= 15 && n >= 0)
 	{
 		for (num = 0; num <= n; ++num)
 		{
 			_putchar(48);
-			for (mult = 0; mult <= n; ++mult)
+			for (mult = 1; mult <= n; ++mult)
 			{
 				_putchar(',');
 				_putchar(' ');
