cfa2953529ef4a52ebd387d0f216ff9cf2eb61db
iAmG-r00t <numimickey2@gmail.com>
2022-02-17 05:32:19 +0300

    fix code logic, minor edit 🐙😀😭👌

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,11 +12,9 @@
 int main(void)
 {
 	int count;
-	unsigned long fib1 = 1, fib2 = 2, sum;
+	unsigned long fib1 = 0, fib2 = 1, sum;
 
-	printf("%lu, %lu, ", fib1, fib2); /*print the first two digits*/
-
-	for (count = 0; count < 48; ++count)
+	for (count = 0; count < 50; ++count)
 	{
 		sum = fib1 + fib2;
 		printf("%lu", sum);
