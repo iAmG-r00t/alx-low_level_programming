cb13381360a43b1e548d7adbd377628b77f08e0a
iAmG-r00t <numimickey2@gmail.com>
2022-02-17 09:36:05 +0300

    fix betty... 🐙😀😭👌

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 98 % 2;
-        positive_or_negative(i);
+	i = 98 % 2;
+	positive_or_negative(i);
 
-        return (0);
+	return (0);
 }
